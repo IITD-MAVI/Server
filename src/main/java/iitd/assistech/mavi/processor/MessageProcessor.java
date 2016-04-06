1b165ee4769169c02b2d05fe7eca111ee2651c37
nipungupta09 <nipungupta.iitd@gmail.com>
2016-04-07 00:16:27 +0530

    All CRUD operations in MEssageProcessor

--- src/main/java/iitd/assistech/mavi/processor/MessageProcessor.java
+++ src/main/java/iitd/assistech/mavi/processor/MessageProcessor.java
@@ -1,8 +1,12 @@
 package iitd.assistech.mavi.processor;
 
+import java.util.List;
+
 import iitd.assistech.mavi.utility.JsonPath;
 import iitd.assistech.mavi.utility.JsonUtil;
 
+import org.springframework.data.mongodb.core.query.BasicUpdate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -58,15 +62,45 @@ public class MessageProcessor {
 	}
 	
 	private static String read(JsonPath jsonPath) {
-		
+		LOG.debug("Query String: " + JsonUtil.toJson(jsonPath.read(QUERY_PATH)));
+		BasicQuery query = new BasicQuery(JsonUtil.toJson(jsonPath.read(QUERY_PATH)));
+		List<String> results =  mongoTemplate.find(query, String.class, jsonPath.read(COLLECTION_PATH));
+		LOG.debug("Number of documents retreived: " + results.size());
+		return JsonUtil.toJson(results);
 	}
 
 	private static String update(JsonPath jsonPath) {
+		LOG.debug("Query String: " + JsonUtil.toJson(jsonPath.read(QUERY_PATH)));
+		BasicQuery query = new BasicQuery(JsonUtil.toJson(jsonPath.read(QUERY_PATH)));
+		List<String> results = mongoTemplate.find(query, String.class, jsonPath.read(COLLECTION_PATH));
+		LOG.debug("Number of documents retreived: " + results.size());
 		
+		if(results.isEmpty()) {
+			LOG.info("No documents to be updated.");
+		} else {
+			LOG.debug("Update String: " + JsonUtil.toJson(jsonPath.read(UPDATE_PATH)));
+			BasicUpdate update = new BasicUpdate(JsonUtil.toJson(jsonPath.read(UPDATE_PATH)));
+			mongoTemplate.updateMulti(query, update, String.class, jsonPath.read(COLLECTION_PATH));
+			LOG.info("Documents updated in MongoDB.");
+		}
+		
+		return JsonUtil.toJson(results);
 	}
 	
 	private static String delete(JsonPath jsonPath) {
+		LOG.debug("Query String: " + JsonUtil.toJson(jsonPath.read(QUERY_PATH)));
+		BasicQuery query = new BasicQuery(JsonUtil.toJson(jsonPath.read(QUERY_PATH)));
+		List<String> results = mongoTemplate.find(query, String.class, jsonPath.read(COLLECTION_PATH));
+		LOG.debug("Number of documents retreived: " + results.size());
 		
+		if(results.isEmpty()) {
+			LOG.info("No documents to be deleted.");
+		} else {
+			mongoTemplate.remove(query, jsonPath.read(COLLECTION_PATH));
+			LOG.info("Documents deleted from MongoDB.");
+		}
+		
+		return JsonUtil.toJson(results);
 	}
 	
 }
