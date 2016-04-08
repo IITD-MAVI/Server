b4f5eb82b92c585982934ebc8f304943dc253605
nipungupta09 <nipungupta.iitd@gmail.com>
2016-04-08 06:49:12 +0530

    Signboard class created & data collected

--- src/main/java/iitd/assistech/mavi/domain/SignBoard.java
+++ src/main/java/iitd/assistech/mavi/domain/SignBoard.java
@@ -7,4 +7,28 @@ import org.springframework.roo.addon.json.RooJson;
 @RooToString
 @RooJson
 public class SignBoard {
+
+    /**
+     */
+    private double latitude;
+
+    /**
+     */
+    private double longitude;
+
+    /**
+     */
+    private Boolean bilingual;
+
+    /**
+     */
+    private Boolean mounted;
+
+    /**
+     */
+    private String engContent;
+
+    /**
+     */
+    private String hinContent;
 }
